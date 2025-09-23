9a737e1b01d0e84c470f9185c2ea2d0267d27af3
comolas <comolasfresas@gmail.com>
2025-09-23 13:57:09 +0300

    Otomatik güncelleme butonu eklendi.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -21,7 +21,7 @@ const LoginPage: React.FC<LoginPageProps> = ({ isDarkMode }) => {
   const navigate = useNavigate();
   const { isAdmin } = useAuth();
   const [animatedTitle, setAnimatedTitle] = React.useState('');
-  const fullTitle = 'Kütüphanemize Hoş Geldiniz';
+  const fullTitle = ' Kütüphanemize Hoş Geldiniz ';
 
   React.useEffect(() => {
     let i = 0;
